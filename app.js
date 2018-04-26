f09e5af04baf4a87380ec899ef7117d3d115515b
Ryan Andersen <ryan.andersen117@gmail.com>
2018-04-26 10:26:18 -0600

    finished initial buildout

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const path = require('path');
 const bParse = require('body-parser');
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+
+const userSchema = mongoose.Schema({
+    userName: String,
+    email: String,
+    age: Number,
+    id: String
+});
+
+const User = mongoose.model('User', userSchema)
+
+mongoose.connect('mongodb://localhost/users');
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+    console.log('successfully connected to mongo');
+
+});
 
 let app = express();
 
@@ -8,8 +28,9 @@ app.use(bParse.json());
 app.use(bParse.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-let idCounter = 0;
-let users = [{userName: 'Mr.Timn', email: 'TheTimster@Tim.com', age: 34, id: 21212}, {userName: 'JoJo', email: 'IsThatAJoJo@reference.com', age: 22, id:4321}];
+let users;
+setUsers();
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,36 +48,71 @@ app.get('/user-edit/:userId', (req, resp) => {
     resp.render('form', {user: findUserById(req.params.userId)});
 });
 app.get('/user-delete/:userId', (req, resp) => {
-    for(let i = 0; i < users.length; i++){
-        if(users[i].id == req.params.userId){
-            users.splice(i, 1);
-        }
-    }
-    resp.redirect('/');
+    User.deleteOne({id: req.params.userId})
+    .then(() => setUsers())
+    .then(() => resp.redirect('/'));
+});
+
+app.get('/sort-ascend', (req, resp) => {
+    User.find((err, usersDb) => {
+        if(err) return console.error(err);
+        users = usersDb.sort((a, b) => a.userName > b.userName);
+    })
+    .then(() => resp.redirect('/'));
+});
+
+app.get('/sort-descend', (req, resp) => {
+    User.find((err, usersDb) => {
+        if(err) return console.error(err);
+        users = usersDb.sort((a, b) => a.userName < b.userName);
+    })
+    .then(() => resp.redirect('/'));
+});
+
+app.get('/reload', (req, resp) => {
+    setUsers().then(() => resp.redirect('/'));
 });
 
 app.post('/create', (req, resp) => {
-    let user = {
+    let user = new User({
         userName: req.body.name,
         email: req.body.email,
         age: req.body.age,
-        id: idCounter++
-    };
-    users.push(user);
-    resp.redirect('/');
+        id: uuidv1()
+    });
+    
+    user.save((err, user) => {
+        if(err) return console.error(err);
+        setUsers().then(() => resp.redirect('/'));
+    })
 });
 app.post('/edit/:userId', (req, resp) => {
-    for(let i = 0; i < users.length; i++){
-        if(users[i].id == req.params.userId){
-            users[i].age = req.body.age;
-            users[i].email = req.body.email;
-            users[i].name = req.body.name;
-        }
-    }
-    resp.redirect('/');
+    User.where({id: req.params.userId}).update({
+        age: req.body.age,
+        email: req.body.email,
+        userName: req.body.name
+    })
+    .then(() => setUsers())
+    .then(() => resp.redirect('/'));
+});
+
+app.post('/search', (req, resp) => {
+    User.find({userName: req.body.searchInput}, (err, usersDb) => {
+        if(err) return console.error(err);
+        users = usersDb;
+    })
+    .then(() => resp.redirect('/'));
 });
 app.listen(3000, () => console.log('server listening on port 3000'));
 
+
 function findUserById(id){
     return users.filter(user => id == user.id)[0];
+}
+
+function setUsers(obj){
+   return User.find(obj, (err, userDb) => {
+        if(err) return console.error(err);
+        users = userDb;
+    });
 }
